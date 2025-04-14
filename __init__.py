7fe6f0f70c1aa607c2368f2a4aaf8fa72e357400
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-04-14 10:56:22 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
+from .nodes import LoadHiDreamModel, GenerateHiDreamImage, SaveHiDreamImage
 
+NODE_CLASS_MAPPINGS = {
+    "LoadHiDreamModel": LoadHiDreamModel,
+    "GenerateHiDreamImage": GenerateHiDreamImage,
+    "SaveHiDreamImage": SaveHiDreamImage,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadHiDreamModel": "Load HiDream Model",
+    "GenerateHiDreamImage": "Generate HiDream Image",
+    "SaveHiDreamImage": "Save HiDream Image",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
